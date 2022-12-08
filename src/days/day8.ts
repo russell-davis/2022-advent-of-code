89128d4e99b49bae92d251b556832d0c358c3465
Luke Davis <dev@rlukedavis.com>
2022-12-08 12:01:37 -0600

    day 8 part2

--- src/days/day8.ts
+++ src/days/day8.ts
@@ -30,7 +30,6 @@ export const day8 = () => {
   const day = 8;
   const file = readTextFile(`day${day}.txt`);
   const lines = file.split("\r\n").filter((line) => line.length > 0);
-  // console.info(`day${day}`, file);
 
   const rows: number[][] = [];
   for (const line of lines) {
@@ -41,6 +40,7 @@ export const day8 = () => {
 
   const seenFromOutside: { row: number; col: number }[] = [];
 
+  // Q1
   for (let rowIndex = 0; rowIndex < rows.length; rowIndex++) {
     const row = rows[rowIndex];
     if (row === undefined) {
@@ -64,12 +64,78 @@ export const day8 = () => {
   }
 
   // Q2
+  const calcScenicScore = (
+    toLeft: number,
+    toRight: number,
+    toTop: number,
+    toBottom: number
+  ) => {
+    return toLeft * toRight * toTop * toBottom;
+  };
+
+  const scenicScores: { row: number; col: number; score: number }[] = [];
+  for (let rowIndex = 0; rowIndex < rows.length; rowIndex++) {
+    const row = rows[rowIndex];
+    if (row === undefined) {
+      continue;
+    }
+    for (let columnIndex = 0; columnIndex < row.length; columnIndex++) {
+      const column = row[columnIndex];
+      if (column === undefined) {
+        continue;
+      }
+      if (onEdge(columnIndex, rowIndex, row.length, rows.length)) {
+        continue;
+      }
+
+      // current val larger or equal to how many values to left in row
+      const currentVal = rows[rowIndex]![columnIndex]!;
+
+      const toLeft = rows[rowIndex]!.slice(0, columnIndex);
+      const toTop = rows.slice(0, rowIndex).map((row) => row[columnIndex]!);
+      const toRight = rows[rowIndex]!.slice(columnIndex + 1);
+      const toBottom = rows.slice(rowIndex + 1).map((row) => row[columnIndex]!);
+
+      const reducerKeeper = (list: number[]) => {
+        let isDone = false;
+        const reducer = (acc, size) => {
+          if (isDone) {
+            return acc;
+          }
+          if (size >= currentVal) {
+            isDone = true;
+            return acc + 1;
+          }
+          if (size < currentVal) {
+            return acc + 1;
+          }
+          return acc;
+        };
+        return list.reduce(reducer, 0) as number;
+      };
+      const canSeeValuesToLeft = reducerKeeper(toLeft.reverse());
+      const canSeeValuesToTop = reducerKeeper(toTop.reverse());
+      const canSeeValuesToRight = reducerKeeper(toRight);
+      const canSeeValuesToBottom = reducerKeeper(toBottom);
+
+      const score = calcScenicScore(
+        canSeeValuesToLeft,
+        canSeeValuesToTop,
+        canSeeValuesToRight,
+        canSeeValuesToBottom
+      );
+      // console.info(`[${rowIndex}, ${columnIndex}] = ${score}`);
+      scenicScores.push({ row: rowIndex, col: columnIndex, score });
+    }
+  }
 
   return {
     question1: {
       numberOfTreesVisibleFromOutsideOfGrid: seenFromOutside.length,
       totalNumberOfTrees: _.sum(rows.map((row) => row.length)),
     },
-    question2: {},
+    question2: {
+      maxScenicScore: _.maxBy(scenicScores, (s) => s.score),
+    },
   };
 };
